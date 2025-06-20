1b2a7a743c47c1dd24561d49e9ca7a420fe5b9b4
ferico <huangferico@gmail.com>
2025-06-20 21:13:29 +0700

    finish logout feature

--- client/pages/Login.jsx
+++ client/pages/Login.jsx
@@ -6,7 +6,7 @@ import { toast } from 'react-toastify';
 
 const Login = () => {
   const { login } = useAuth();
-  const [isLogin, setIsLogin] = useState(false);
+  const [isLogin, setIsLogin] = useState(true);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
