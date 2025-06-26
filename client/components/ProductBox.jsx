535afb70033e9506b450d71bc4aeff82586fedb8
Ferico5 <huangferico@gmail.com>
2025-06-26 15:58:15 +0700

    add component product box

---
+++ client/components/ProductBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const ProductBox = ({ image, name, price }) => {
+  return (
+    <div className="flex flex-col mb-3 hover:cursor-pointer">
+      {/* Product Image */}
+      <div className="bg-gray-100 overflow-hidden">
+        <img 
+          src={image} 
+          alt={name} 
+          className="w-full h-full object-cover" 
+        />
+      </div>
+
+      {/* Product Name */}
+      <p className="mt-2 text-[#374151] break-words leading-tight">
+        {name}
+      </p>
+
+      {/* Product Price */}
+      <p className="text-sm text-[#374151] font-medium">
+        Rp. {price}
+      </p>
+    </div>
+  );
+}
+
+export default ProductBox
--- client/pages/Collection.jsx
+++ client/pages/Collection.jsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import CategoryBox from '../components/CategoryBox'
+import ProductBox from '../components/ProductBox';
+import p_img1 from '../assets/frontend_assets/p_img2_1.png'
 
 const Collection = () => {
+  const [sortOption, setSortOption] = useState('Relevent');
+
   return (
     <div className='content border-t border-[#E5E7EB] pt-9 flex justify-between font-outfit'>
       {/* Category */}
@@ -20,7 +25,29 @@ const Collection = () => {
     
       {/* Products */}
       <div className='w-3/4'>
-        <p>products</p>
+        <div className='flex justify-between text-2xl text-[#707070] mb-5'>
+          <p>ALL <span className='text-[#171717] font-[470]'>COLLECTIONS</span></p>
+          
+          {/* Sort Dropdown */}
+          <select
+            value={sortOption}
+            onChange={(e) => setSortOption(e.target.value)}
+            className='border border-gray-300 text-sm px-3 py-2 rounded shadow-sm focus:outline-none focus:ring-2 focus:ring-black'
+          >
+            <option value="Relevent">Sort by: Relevent</option>
+            <option value="LowToHigh">Sort by: Low to High</option>
+            <option value="HighToLow">Sort by: High to Low</option>
+          </select>
+        </div>
+
+        {/* Product List */}
+        <div className="grid grid-cols-4 gap-3">
+          <ProductBox 
+            image={p_img1}
+            name='tesssssssssssssssssssssssssssssssssssss'
+            price='30000'
+          />
+        </div>
       </div>
     </div>
   )
