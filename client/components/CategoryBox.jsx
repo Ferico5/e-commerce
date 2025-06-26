978341009bbc0e4c733867ca4b971e81b37103d7
Ferico5 <huangferico@gmail.com>
2025-06-26 15:09:12 +0700

    add category

---
+++ client/components/CategoryBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CategoryBox = ({ title, categories = [], className='' }) => {
+  return (
+    <div className={`flex flex-col border border-[#E5E7EB] py-3 pl-4 ${className}`}>
+      <span className="mb-3 text-sm">{title}</span>
+
+      {categories.map((category, index) => (
+        <label key={index} className="flex items-center gap-2 mb-1">
+          <input type="checkbox" className="accent-blue-500" />
+          <span className='text-[#374151] text-sm'>{category}</span>
+        </label>
+      ))}
+    </div>
+  );
+};
+
+export default CategoryBox;
--- client/pages/Collection.jsx
+++ client/pages/Collection.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import CategoryBox from '../components/CategoryBox'
 
 const Collection = () => {
   return (
-    <div>Collection</div>
+    <div className='content border-t border-[#E5E7EB] pt-9 flex justify-between font-outfit'>
+      {/* Category */}
+      <div className='w-[27.5%] pr-10 pt-1'>
+        <span className='text-xl'>FILTERS</span>
+        <CategoryBox
+          title='CATEGORIES'
+          categories={['Men', 'Women', 'Kids']}
+          className='mb-5 mt-6'
+        />
+
+        <CategoryBox 
+          title='TYPE'
+          categories={['Topwear', 'Bottomwear', 'Winterwear']}
+        />
+      </div>
+    
+      {/* Products */}
+      <div className='w-3/4'>
+        <p>products</p>
+      </div>
+    </div>
   )
 }
 
