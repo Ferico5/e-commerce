d1a828507fb748e8c286d3f9a19152594783fe00
ferico <huangferico@gmail.com>
2025-07-29 14:48:40 +0700

    finish UI AdminOrder

--- client/pages/admin/AdminOrders.jsx
+++ client/pages/admin/AdminOrders.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useAuth } from '../../auth/AuthContext.jsx';
+import parcel_icon from '../../assets/admin_assets/parcel_icon.svg';
+import axios from '../../utils/axiosInstance.js';
 
 const AdminOrders = () => {
-  return (
-    <div>AdminOrders</div>
-  )
-}
+  const { token } = useAuth();
+  const [orders, setOrders] = useState([]);
 
-export default AdminOrders
+  useEffect(() => {
+    const fetchUsersOrders = async () => {
+      try {
+        const res = await axios.get(`orders`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setOrders(res.data.orders);
+      } catch (error) {
+        console.error('Failed to fetch orders:', error);
+      }
+    };
+
+    fetchUsersOrders();
+  }),
+    [token];
+
+  return (
+    <div className="font-outfit text-[#4B5563]">
+      <p className="mb-3">Order Page</p>
+      <div className="flex flex-col gap-3">
+        {orders.map((order) => {
+          const totalItems = order.items.reduce((acc, item) => acc + item.quantity, 0);
+          const formattedDate = new Date(order.date).toLocaleDateString('id-ID', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+          });
+
+          return (
+            <div key={order._id} className="flex justify-between items-start border border-[#E5E7EB] px-4 py-5">
+              <img src={parcel_icon} className="w-13" />
+              <div className="w-2/6">
+                {order.items.map((item, index) => (
+                  <p key={index}>
+                    {item.name} x {item.quantity} {item.size},
+                  </p>
+                ))}
+
+                <p className="my-2">{order.userName}</p>
+                <p>{order.street},</p>
+                <p>{order.city}, {order.state}, {order.country}, {order.zipcode}</p>
+                <p>{order.phone}</p>
+              </div>
+
+              <div className="w-1/6">
+                <p className="mb-2">Items: {totalItems}</p>
+                <p>Method: <span className='uppercase'>{order.paymentMethod}</span></p>
+                <p>Payment: {order.payment ? 'Paid' : 'Pending'}</p>
+                <p>Date: {formattedDate}</p>
+              </div>
+
+              <div className="w-1/6 flex justify-center">
+                <p>Rp {order.total_fee.toLocaleString('id-ID')}</p>
+              </div>
+
+              <select className="w-1/6 border border-[#E5E7EB] hover:cursor-pointer px-3 py-1" defaultValue={order.status}>
+                <option value="Order Placed">Order Placed</option>
+                <option value="Packing">Packing</option>
+                <option value="Out for Delivery">Out for Delivery</option>
+                <option value="Delivered">Delivered</option>
+              </select>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default AdminOrders;
