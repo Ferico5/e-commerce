964134b0df2ebfbf7135ac01ab1fa9032c97a4a9
ferico <huangferico@gmail.com>
2025-08-26 20:53:53 +0700

    add responsive container

---
+++ client/components/ResponsiveContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+const ResponsiveContainer = ({ children, className = '' }) => {
+  const isMobile = useMediaQuery({ maxWidth: 639 });
+  const isTablet = useMediaQuery({ minWidth: 640, maxWidth: 1023 });
+  const isLaptop = useMediaQuery({ minWidth: 1024, maxWidth: 1279 });
+
+  const baseClass = 'font-outfit';
+
+  const deviceClass = isMobile ? 'content-mobile' : isTablet ? 'content-tablet' : isLaptop ? 'content-laptop' : 'content-desktop';
+
+  return <div className={`${deviceClass} ${baseClass} ${className}`}>{children}</div>;
+};
+
+export default ResponsiveContainer;
