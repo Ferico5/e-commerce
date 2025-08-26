202b6d3d578c2ee44f8cf72d840e39285ae2540c
ferico <huangferico@gmail.com>
2025-08-26 21:32:15 +0700

    responsive mobile

--- client/pages/user/Collection.jsx
+++ client/pages/user/Collection.jsx
@@ -3,6 +3,7 @@ import axios from '../../utils/axiosInstance';
 import CategoryBox from '../../components/user/CategoryBox';
 import ProductBox from '../../components/user/ProductBox';
 import TitleBox from '../../components/user/TitleBox';
+import ResponsiveContainer from '../../components/ResponsiveContainer.jsx';
 import { useLocation } from 'react-router-dom';
 import Search from '../../assets/frontend_assets/search_icon.png';
 import Close from '../../assets/frontend_assets/cross_icon.png';
@@ -16,6 +17,7 @@ const Collection = () => {
   const [showSearch, setShowSearch] = useState(location.state?.showSearch || false);
   const [searchTerm, setSearchTerm] = useState('');
   const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
+  const [openFilters, setOpenFilters] = useState(false);
 
   useEffect(() => {
     axios
@@ -77,10 +79,10 @@ const Collection = () => {
   };
 
   return (
-    <div className={`content border-t border-[#E5E7EB] flex flex-col justify-between font-outfit ${showSearch ? `` : `pt-9`}`}>
+    <ResponsiveContainer className={`border-t border-[#E5E7EB] flex flex-col justify-between ${showSearch ? `` : `pt-9`}`}>
       {showSearch && (
         <div className="bg-[#F9FAFB] border-t border-b border-[#E5E7EB] mb-4 py-5 flex justify-center">
-          <div className="flex items-center gap-2 w-1/2">
+          <div className="flex items-center gap-2 sm:w-1/2">
             <div className="relative flex-grow">
               <input type="text" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className="w-full px-4 py-2 pr-10 text-sm border border-[#9CA3AF] rounded-full focus:outline-none" />
               <img src={Search} className="w-4 absolute right-3 top-1/2 transform -translate-y-1/2 pointer-events-none" />
@@ -98,18 +100,27 @@ const Collection = () => {
         </div>
       )}
 
-      <div className="flex">
-        {/* Category */}
-        <div className="w-[27.5%] pr-10 pt-1">
-          <span className="text-xl">FILTERS</span>
-          <CategoryBox title="CATEGORIES" categories={['Men', 'Women', 'Kids']} selected={selectedCategories} onChange={handleCategoryChange} className="mb-5 mt-6" />
+      <div className="flex flex-col sm:flex-row">
+        {/* FILTERS */}
+        <div className="sm:w-[27.5%] sm:pr-10 pt-1">
+          {/* Header Filter */}
+          <button className="flex items-center w-full sm:block sm:text-left" onClick={() => setOpenFilters(!openFilters)}>
+            <span className="text-xl">FILTERS</span>
+            {/* Panah cuma muncul di mobile */}
+            <span className="sm:hidden text-lg ml-2">{openFilters ? '▲' : '▼'}</span>
+          </button>
 
-          <CategoryBox title="TYPE" categories={['Topwear', 'Bottomwear', 'Winterwear']} selected={selectedSubCategories} onChange={handleSubCategoryChange} />
+          {/* Categories (Mobile Accordion) */}
+          <div className={`${openFilters ? 'block' : 'hidden'} sm:block`}>
+            <CategoryBox title="CATEGORIES" categories={['Men', 'Women', 'Kids']} selected={selectedCategories} onChange={handleCategoryChange} className="mb-5 mt-6" />
+
+            <CategoryBox title="TYPE" categories={['Topwear', 'Bottomwear', 'Winterwear']} selected={selectedSubCategories} onChange={handleSubCategoryChange} />
+          </div>
         </div>
 
         {/* Products */}
-        <div className="w-3/4">
-          <div className="flex justify-between text-2xl text-[#707070] mb-5">
+        <div className="sm:w-3/4">
+          <div className="flex justify-between text-2xl text-[#707070] mb-5 mt-5 sm:mt-0">
             <TitleBox first="ALL" second="COLLECTIONS" size="big" />
 
             {/* Sort Dropdown */}
@@ -121,14 +132,14 @@ const Collection = () => {
           </div>
 
           {/* Product List */}
-          <div className="grid grid-cols-4 gap-3">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
             {getSortedProducts().map((product) => (
               <ProductBox key={product._id} id={product._id} image={product.image[0]} name={product.name} price={product.price} />
             ))}
           </div>
         </div>
       </div>
-    </div>
+    </ResponsiveContainer>
   );
 };
 
