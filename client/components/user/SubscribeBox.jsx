52b9ed825c72193230875ebd5628d9a559dea8ca
ferico <huangferico@gmail.com>
2025-08-27 19:31:31 +0700

    responsive all devices

--- client/components/user/SubscribeBox.jsx
+++ client/components/user/SubscribeBox.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const SubscribeBox = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center p-10 font-outfit">
-      <p className="font-[600] text-gray-800 text-2xl mb-3">Subscribe now & get 20% off</p>
-      <p className="text-[#9CA3AF]">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+      <p className="font-[600] text-gray-800 text-center text-2xl mb-3">Subscribe now & get 20% off</p>
+      <p className="text-[#9CA3AF] text-sm sm:text-base text-center">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
       <div className="mt-5 w-full flex flex-row items-center justify-center">
-        <input type="text" className="w-2/5 h-11 px-3 py-6 border-gray-300 border-1 font-outfit" placeholder="Enter your email" autoComplete="off" />
+        <input type="text" className="w-3/5 sm:2/5 h-11 px-3 py-6 border-gray-300 border-1 font-outfit text-sm sm:text-base" placeholder="Enter your email" autoComplete="off" />
         <button className="w-34 h-12.5 py-3 bg-[#000] text-[#FFF] text-[11px] font-outfit hover:cursor-pointer">SUBSCRIBE</button>
       </div>
     </div>
--- client/components/user/TitleBox.jsx
+++ client/components/user/TitleBox.jsx
@@ -1,38 +1,43 @@
-const TitleBox = (props) => {
-  function TitleSize() {
-    if (props.size === "big") {
-      return (
-        <div className="flex justify-center items-center font-medium text-3xl text-[#707070] mb-2 gap-2">
-          <p>
-            {props.first}{" "}
-            <span className="text-[#171717] font-[500] "> {props.second}</span>
-          </p>
-          <span className="w-12 h-[2px] bg-[#171717]"></span>
-        </div>
-      );
-    } else if (props.size === "small") {
-      return (
-        <div className="flex items-center font-medium text-xl text-[#707070] mb-10 gap-2">
-          <p>
-            {props.first}{" "}
-            <span className="text-[#171717] font-[470]"> {props.second}</span>
-          </p>
-          <span className="w-12 h-[2px] bg-[#171717]"></span>
-        </div>
-      );
-    } else {
-      return (
-        <div className="flex justify-center items-center font-medium text-2xl text-[#707070] mb-13 gap-2">
-          <p>
-            {props.first}{" "}
-            <span className="text-[#171717] font-[470]"> {props.second}</span>
-          </p>
-          <span className="w-12 h-[2px] bg-[#171717]"></span>
-        </div>
-      );
-    }
+const TitleBox = ({ first, second, size }) => {
+  const baseClasses =
+    "flex justify-center items-center text-[#707070] mb-2 gap-2";
+  const spanClasses = "text-[#171717] font-[500]";
+  const line = <span className="w-12 h-[2px] bg-[#171717]"></span>;
+
+  if (size === "big") {
+    return (
+      <div
+        className={`${baseClasses} font-medium text-sm sm:text-2xl md:text-3xl`}
+      >
+        <p>
+          {first} <span className={spanClasses}>{second}</span>
+        </p>
+        {line}
+      </div>
+    );
+  } else if (size === "small") {
+    return (
+      <div
+        className="flex items-center font-medium text-base sm:text-lg md:text-xl text-[#707070] mb-10 gap-2"
+      >
+        <p>
+          {first} <span className="text-[#171717] font-[470]">{second}</span>
+        </p>
+        {line}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="flex justify-center items-center font-medium text-lg sm:text-xl md:text-2xl text-[#707070] mb-13 gap-2"
+      >
+        <p>
+          {first} <span className="text-[#171717] font-[470]">{second}</span>
+        </p>
+        {line}
+      </div>
+    );
   }
-  return TitleSize();
 };
 
 export default TitleBox;
