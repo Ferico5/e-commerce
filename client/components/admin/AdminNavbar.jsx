3abe98e3d363157783fa200d71c9f5c4e3d845a9
ferico <huangferico@gmail.com>
2025-09-12 19:42:45 +0700

    responsive all devices

--- client/components/admin/AdminNavbar.jsx
+++ client/components/admin/AdminNavbar.jsx
@@ -11,29 +11,38 @@ const AdminNavbar = () => {
   if (!token || !user || user.role !== 'admin') return null;
 
   return (
-    <div className="flex justify-end w-[50%] sm:w-[39%] md:w-[32%] lg:w-[24%] xl:w-[19%] h-screen border-r-2 border-[#E5E7EB] bg-[#F9FAFB]">
-      <ul className="font-outfit w-[78%]">
+    <div className="flex justify-end w-[15%] md:w-[23%] lg:w-[24%] xl:w-[19%] h-screen border-r-2 border-[#E5E7EB] bg-[#F9FAFB]">
+      <ul className="font-outfit w-[80%] md:w-[78%]">
         {/* Add */}
         <li>
-          <Link to="add" className={`flex gap-3 pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] ${location.pathname === '/admin/add' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}>
+          <Link
+            to="add"
+            className={`flex gap-3 md:pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] justify-center md:justify-start ${location.pathname === '/admin/add' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}
+          >
             <img src={add_icon} className="w-5 h-5" />
-            Add Items
+            <p className="hidden md:inline">Add Items</p>
           </Link>
         </li>
 
         {/* Lists */}
         <li>
-          <Link to="list" className={`flex gap-3 pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] ${location.pathname === '/admin/list' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}>
+          <Link
+            to="list"
+            className={`flex gap-3 md:pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] justify-center md:justify-start ${location.pathname === '/admin/list' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}
+          >
             <img src={order_icon} className="w-5 h-5" />
-            List Items
+            <p className="hidden md:inline">List Items</p>
           </Link>
         </li>
 
         {/* Orders */}
         <li>
-          <Link to="orders" className={`flex gap-3 pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] ${location.pathname === '/admin/orders' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}>
+          <Link
+            to="orders"
+            className={`flex gap-3 md:pl-3 py-2 mt-6 border-b border-t border-l rounded-l-md text-[0.938rem] justify-center md:justify-start ${location.pathname === '/admin/orders' ? 'bg-[#FFEBF5] border-[#C586A5]' : 'border-[#E5E7EB]'}`}
+          >
             <img src={order_icon} className="w-5 h-5" />
-            Orders
+            <p className="hidden md:inline">Orders</p>
           </Link>
         </li>
       </ul>
